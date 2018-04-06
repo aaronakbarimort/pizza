fd904fb4e220168f9bbb5fcecd80f6bb1c52e59b
Aaron Akbari-Mort <aaronakbarimort@gmail.com>
2018-04-06 10:20:01 -0700

    add inital Pizza contructor and Pizza object with toppings & size options

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 // Business Logic //
+function Pizza(toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+}
 
-
+var pizza = new Pizza(["cheese", "pepperoni", "artichoke", "anchovy", "mushroom", "sausage"], ["small", "large"]);
 
 // User Interface Logic //
 $(document).ready(function() {
